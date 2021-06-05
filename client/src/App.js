31356319708a3766af9e1fd06cc938573b5e4a04
rikuma0 <riku.manninen@icloud.com>
2021-06-05 13:14:09 +0300

    leaflet.js setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import React, { useState } from 'react';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
 
+const position = [61.089, 24.12884]
+
 function App() {
-
-  const [hello, setHello] = useState('');
-  
-  fetch("api/hello")
-  .then(res => res.text())
-  .then(res => setHello(res))
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {hello}
-        </p>
-      </header>
-    </div>
+    <MapContainer center={position} zoom={20} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          Iittala Village
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
