c21dd7a219d95f83779f2ae32a8634ce7ab30d09
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2021-06-07 15:39:05 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ const position = [61.089, 24.12884]
 
 function App() {
   return (
-    <MapContainer center={position} zoom={20} scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={20} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
