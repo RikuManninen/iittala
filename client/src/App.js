c24f401f1e515aa9d6356871c4bb2b0d1808a9e6
rikuma0 <riku.manninen@icloud.com>
2021-06-09 08:13:49 +0300

    leaflet map as component

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Map from './Map';
 import './App.css';
 
-const position = [61.089, 24.12884]
 
 function App() {
   return (
-    <MapContainer center={position} zoom={20} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          Iittala Village
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <Map />
   );
 }
 
