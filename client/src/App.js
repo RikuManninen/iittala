3bc22b3bfb20f70479fbaba938b525f9a2cab393
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2021-06-09 12:20:22 +0300

    Tabin lisääminen

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import './App.css';
+import React, { Component } from 'react';
+import {App3, App2 } from "./Tab"; 
 
-const position = [61.089, 24.12884]
 
-function App() {
-  return (
-    <MapContainer center={position} zoom={20} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          Iittala Village
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
+
+
+class App extends Component {
+  render(){
+    return(
+      <div className="container">
+        <App3/>
+        <App2/>
+      </div>
+    )
+  }
 }
 
 export default App;
