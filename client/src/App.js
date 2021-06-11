5197d37e94b6dac8b2151dba70f4924493a24133
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2021-06-11 16:23:20 +0300

    Parempi tabi systeemi ja jotenkuten toimii toi mappi myös

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,16 @@
-import { Dropdown } from "react-bootstrap";
 import React, { Component } from 'react';
-import Kalkkuna from "./Tabit"; 
-import Map from './Map';
+import Tabs from './Tabit';
 
 
 
 
-class App extends Component {
-  render(){
-    return(
-      
-        
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-    
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1"><Kalkkuna/></Dropdown.Item>
-        <Dropdown.Item href="#/action-2"><Map/></Dropdown.Item>
-        
-      </Dropdown.Menu>
-    </Dropdown>
-      
-    )
-  }
+function App() {
+
+  return (
+    <div className="App">
+        <Tabs />
+    </div>
+  );
 }
-
+    
 export default App;
--- client/src/Tabit.js
+++ client/src/Tabit.js
@@ -1,25 +1,72 @@
-import React, { Component } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Map from "./Map";
 
-export default class Kalkkuna extends Component {
-  render() {
-    
-      return(
-  <Tabs>
-    <TabList>
-      <Tab>Title 1</Tab>
-      <Tab>Title 2</Tab>
-    </TabList>
+function Tabs() {
+  const [toggleState, setToggleState] = useState(1);
 
-    <TabPanel>
-      <h2>Any content 1</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
-);
-      }
-    }
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <div className="container">
+      <div className="bloc-tabs">
+        <button
+          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(1)}
+        >
+          Tab 1
+        </button>
+        <button
+          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(2)}
+        >
+          Tab 2
+        </button>
+        <button
+          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(3)}
+        >
+          Tab 3
+        </button>
+      </div>
+
+      <div className="content-tabs">
+        <div
+          className={toggleState === 1 ? "content  active-content" : "content"}
+        >
+          <h2>Content 1</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            praesentium incidunt quia aspernatur quasi quidem facilis quo nihil
+            vel voluptatum?
+          </p>
+        </div>
+
+        <div
+          className={toggleState === 2 ? "content  active-content" : "content"}
+        >
+          <h2>Content 2</h2>
+          <hr />
+          <p>
+            <Map/>
+          </p>
+        </div>
+
+        <div
+          className={toggleState === 3 ? "content  active-content" : "content"}
+        >
+          <h2>Content 3</h2>
+          <hr />
+          <p>
+            Tarviin tuleettimen
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Tabs;
