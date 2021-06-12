1381e8faa677383ed9c3c53ecdef225c19fb0436
rikuma0 <riku.manninen@icloud.com>
2021-06-12 17:11:13 +0300

    added location marker with accuracy

--- client/src/Map.js
+++ client/src/Map.js
@@ -1,22 +1,65 @@
-import React, { Component } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { Component, useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap, Circle } from 'react-leaflet'
+import useGeolocation from "react-navigator-geolocation";
 
 const position = [61.089, 24.12884]
 
-export default class Map extends Component {
+class Map extends Component {
     render() {
-        return(
-            <MapContainer center={position} zoom={20} scrollWheelZoom={true}>
+        return (
+            <MapContainer center={ position } zoom={ 20 } scrollWheelZoom={ true }>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker position={ position }>
                     <Popup>
-                    Iittala Village
+                        Iittala Village
                     </Popup>
                 </Marker>
+                <LocationMarker />
             </MapContainer>
         )
     }
+}
+
+function LocationMarker() {
+    
+    const [position, setPosition] = useState(null);
+    const [accuracy, setAccuracy] = useState(null);
+    const map = useMap();
+
+    useEffect(() => {
+        map.locate().on("locationfound", function (e) {
+            setPosition(e.latlng);
+            setAccuracy(e.accuracy);
+        });
+    }, []);
+
+    return position === null ? null : (
+        <div>
+            <Marker position={position}>
+                <Popup>You are here</Popup>
+            </Marker>
+            <Circle center={position} radius={accuracy}></Circle>
+        </div>
+    );
+}
+
+export default () => {
+    const { isEnabled, coords } = useGeolocation();
+    const lat = coords?.latitude;
+    const long = coords?.longitude;
+    return isEnabled ? (
+        <div>
+            <h3>User location: { lat + ', ' + long }</h3>
+            <h4>Accuracy: { coords?.accuracy }</h4>
+            <Map />
+        </div>
+    ) : (
+        <div>
+            <h4>Location permission is not enabled</h4>
+            <Map />
+        </div>
+    ) 
 }
