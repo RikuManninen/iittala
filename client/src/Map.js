6423d5158df60e047170c37c94a714bfaa076eac
rikuma0 <riku.manninen@icloud.com>
2021-06-14 18:32:29 +0300

    list for marker generation w/ some markers for testing

--- client/src/Map.js
+++ client/src/Map.js
@@ -2,21 +2,29 @@ import React, { Component, useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap, Circle } from 'react-leaflet'
 import useGeolocation from "react-navigator-geolocation";
 
-const position = [61.089, 24.12884]
+const positions = 
+[
+    [[61.08900, 24.12884], "talo 1"], 
+    [[61.08940, 24.12903], "talo 2"], 
+    [[61.08990, 24.12870], "talo 3"], 
+    [[61.08945, 24.12797], "talo 4"],
+    [[61.08950, 24.12778], "parkkipaikka 1"],
+    [[61.08878, 24.12820], "parkkipaikka 2"],
+]
 
 class Map extends Component {
     render() {
         return (
-            <MapContainer center={ position } zoom={ 20 } scrollWheelZoom={ true }>
+            <MapContainer center={ positions[0][0] } zoom={ 17 } scrollWheelZoom={ true }>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={ position }>
-                    <Popup>
-                        Iittala Village
-                    </Popup>
-                </Marker>
+                {
+                positions.map((value, index) => {
+                    return <Marker position={value[0]}><Popup>{value[1]}</Popup></Marker>
+                })
+                }
                 <LocationMarker />
             </MapContainer>
         )
