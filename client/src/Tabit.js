b1e67ef2392eb0ada81fce594d8d0aa642a8c4b7
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2021-06-18 14:10:32 +0300

    Uus tabi systeemi joka on responsiivinen

--- client/src/Tabit.js
+++ client/src/Tabit.js
@@ -1,73 +1,35 @@
-import { useState } from "react";
 import "./App.scss";
 import Map from "./Map";
+import {Tab, Tabs, TabList,TabPanel} from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
-function Tabs() {
-  const [toggleState, setToggleState] = useState(1);
 
-  const toggleTab = (index) => {
-    setToggleState(index);
-  };
 
-  return (
-    <div className="container">
-      <div className="bloc-tabs">
-        <button
-          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-          onClick={() => toggleTab(1)}
-        >
-          Tab 1
-        </button>
-        <button
-          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-          onClick={() => toggleTab(2)}
-        >
-          Tab 2
-        </button>
-        <button
-          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
-          onClick={() => toggleTab(3)}
-        >
-          Tab 3
-        </button>
-      </div>
+function Tabits() {
+  return <div>
+    <Tabs>
+  <TabList>
+    <Tab>Map</Tab>
+    <Tab>Information</Tab>
+    
+  </TabList>
 
-      <div className="content-tabs">
-        <div
-          className={toggleState === 1 ? "content  active-content" : "content"}
-        >
-          <h2>Content 1</h2>
-          <hr />
-          <p>
-            <Map/>
-            
-          </p>
-        </div>
-
-        <div
-          className={toggleState === 2 ? "content  active-content" : "content"}
-        >
-          <h2>Content 2</h2>
-          <hr />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-            praesentium incidunt quia aspernatur quasi quidem facilis quo nihil
-            vel voluptatum?
-          </p>
-        </div>
-
-        <div
-          className={toggleState === 3 ? "content  active-content" : "content"}
-        >
-          <h2>Content 3</h2>
-          <hr />
-          <p>
-            Tarviin tuleettimen
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+  <TabPanel>
+    <p>
+      
+      <Map/>
+    </p>
+    
+  </TabPanel>
+  <TabPanel>
+    <p>
+      Tää on placeholder nyt muokkaan css vähän.
+    </p>
+    
+  </TabPanel>
+  
+</Tabs>
+</div>
 }
 
-export default Tabs;
+export default Tabits;
