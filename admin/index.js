330a48b67af202c673f56271a8a09357dcfeeb93
rikuma0 <riku.manninen@icloud.com>
2021-06-23 22:52:26 +0300

    adminbro: richtext editor for marker content

--- admin/index.js
+++ admin/index.js
@@ -1,11 +1,38 @@
 const AdminBro = require('admin-bro');
 const db = require('../models');
+const AdminBroSequelize = require('@admin-bro/sequelize')
+
+AdminBro.registerAdapter(AdminBroSequelize)
+
 const adminBro = new AdminBro({
-   rootPath: '/admin',
-   databases: [db],
-   branding: {
-     companyName: 'Iittala App',
-     softwareBrothers: false,
-   }
+  rootPath: '/admin',
+  databases: [db],
+  branding: {
+    companyName: 'Iittala App',
+    softwareBrothers: false,
+    logo: false,
+  },
+  resources: [
+    { 
+      resource: db.Marker, 
+      options: {
+        icon: 'Marker',
+        parent: {
+          name: 'Map',
+          icon: 'Map',
+        },
+        properties: {
+          content: {
+            type: 'richtext',
+            //components: {
+            //  edit: AdminBro.bundle('./editor'),
+            //}
+          }
+        },
+      } 
+    },
+  ],
 });
+
+
 module.exports = adminBro;
