601b636d5d0b6d956d5da2bd3d69d0a28c7a638b
rikuma0 <riku.manninen@icloud.com>
2021-07-16 16:49:23 +0300

    ar: add browser compatibility component

--- client/src/Ar/index.js
+++ client/src/Ar/index.js
@@ -1,7 +1,7 @@
 import React, { Suspense, useRef } from 'react';
 import { useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three';
-import { FaceBufferGeometry, FaceTracker, ZapparCamera, ZapparCanvas } from '@zappar/zappar-react-three-fiber';
+import { FaceBufferGeometry, FaceTracker, ZapparCamera, ZapparCanvas, BrowserCompatibility } from '@zappar/zappar-react-three-fiber';
 
 import faceMapSrc from './assets/faceMeshTemplate.png'
 
@@ -13,17 +13,20 @@ const FaceMeshMaterial = () => {
 export default () => {
   const trackerGroup = useRef()
   return (
-    <ZapparCanvas>
-      <ZapparCamera userFacing/>
-      <FaceTracker ref={trackerGroup}>
-        <Suspense fallback={null}>
-          <mesh>
-            <FaceMeshMaterial />
-            <FaceBufferGeometry trackerGroup={trackerGroup}/>
-          </mesh>
-        </Suspense>
-      </FaceTracker>
-      <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
-    </ZapparCanvas>
+    <>
+      <BrowserCompatibility />
+      <ZapparCanvas>
+        <ZapparCamera userFacing/>
+        <FaceTracker ref={trackerGroup}>
+          <Suspense fallback={null}>
+            <mesh>
+              <FaceMeshMaterial />
+              <FaceBufferGeometry trackerGroup={trackerGroup}/>
+            </mesh>
+          </Suspense>
+        </FaceTracker>
+        <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
+      </ZapparCanvas>
+    </>
   );
 }
