7c7420976f89f3f0a4e28420fd90e943f8d8305c
rikuma0 <riku.manninen@icloud.com>
2021-07-24 19:34:44 +0300

    modal: games dictionary & refactor

--- client/src/Map/Modal.js
+++ client/src/Map/Modal.js
@@ -7,11 +7,18 @@ import { Redirect, BrowserRouter as Router } from 'react-router-dom';
 
 const Modal = (props) => {
 
+  const markerId = props.markerId
+  const content = props.content
+  const modalIsOpen = props.modalIsOpen
+  const closeModal = props.closeModal
+
 	const [gameIsOpen, setGameIsOpen] = useState(false);
-  const gameMarkerIds = [
-    125, 
-    275
-  ]
+
+  //markerId : game JSX
+  const games = {
+    125 : Ar(1),
+    275 : Ar(2)
+  }
 
 	const openGame = () => {
 		setGameIsOpen(true)
@@ -23,23 +30,26 @@ const Modal = (props) => {
 
 	return(
 		<ReactModal
-			isOpen={ props.modalIsOpen }
+			isOpen={ modalIsOpen }
 			className="modal"
 			overlayClassName="modal-container"
 		>
-      <Router>
+
+      {/* redirects back to root if modal is opened with a url */}
+      <Router> 
         <Redirect to="/" />
       </Router>
+
       {!gameIsOpen ? 
         <>
-          <div dangerouslySetInnerHTML={{ __html: props.content }} />
-          <button className="btn-modal-left" onClick={ props.closeModal }><ArrowBackIcon/></button>
-          {gameMarkerIds.some(id => id === props.markerId) && <button className="btn-modal-right" onClick={ openGame }><VideogameAssetIcon/></button>}
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+          <button className="btn-modal-left" onClick={ closeModal }><ArrowBackIcon/></button>
+          {markerId in games && <button className="btn-modal-right" onClick={ openGame }><VideogameAssetIcon/></button>}
         </>
       : 
         <>
           <div className="modal-game-container">
-            <Ar />
+            {games[markerId]}
           </div>
           <button className="btn-modal-left" onClick={ closeGame }><ArrowBackIcon/></button>
         </>
