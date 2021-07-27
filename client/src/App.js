7cb6b46e7415815be722832ab004621e8e618dc0
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2021-07-27 12:27:55 +0300

    Kyyl

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import Tav from './Tabit';
 import React from 'react';
 import './App.scss';
-import Alas from './Dropdownmenu';
+
 
 
 function App() {
 
   return (
     <div className="App">
-        <Alas />
+        <Tav />
     </div>
     
   );
